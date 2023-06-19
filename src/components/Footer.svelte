d030758e7aaa555b6d9eabc1f1da1424f4f34b53
eangle9 <engdaworkyismaw9@gmail.com>
2023-06-19 13:16:37 +0300

    header component added

---
+++ src/components/Footer.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<footer>
+  <div class="copyright">@copyright is not reserved</div>
+</footer>
+
+<style>
+    footer{
+       margin: 40px auto;
+        border-top: 1px solid #777;
+        text-align: center;
+        max-width: 300px;
+        padding-top: 10px;
+
+    }
+    .copyright{
+        color: #666;
+        font-size: 18px;
+    }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<header>
+  <h1><img src="/images/logo.jpg" alt="logo" /></h1>
+</header>
+
+<style>
+header{
+    background: #ccc;
+    padding: 40px;
+    text-align: center;
+}
+img{
+    width: 100px;
+}
+</style>
