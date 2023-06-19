f3a5f8d478586f4a42e89a9cecb82a271432f3e5
eangle9 <engdaworkyismaw9@gmail.com>
2023-06-19 13:19:24 +0300

    chore:button component added

---
+++ src/shared/Button.svelte
@@ -0,0 +1,51 @@
+<script>
+  export let type = "primary";
+  export let inverse = false;
+</script>
+
+<button class={type} class:reverse={inverse}>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    margin-top: 10px;
+    padding: 8px 12px;
+    border-radius: 6px;
+    cursor: pointer;
+  }
+  .primary {
+    background-color: darkturquoise;
+    color: #fff;
+  }
+  .primary:focus{
+    background-color: rgb(15, 85, 85);
+    color: #fff;
+  }
+  .primary:hover{
+    background-color: darkcyan;
+  }
+  .secondary{
+    background-color: dodgerblue;
+    color: #fff;
+  }
+  .error{
+    background-color: red;
+    color: #fff;
+  }
+  .primary.reverse{
+    background-color: #fff;
+    color: darkturquoise;
+    border: 1px solid darkturquoise;
+  }
+  .secondary.reverse{
+    background-color: #fff;
+    color: dodgerblue;
+    border: 1px solid dodgerblue;
+  }
+  .error.reverse{
+    background-color: #fff;
+    color: red;
+    border: 1px solid red;
+  }
+</style>
